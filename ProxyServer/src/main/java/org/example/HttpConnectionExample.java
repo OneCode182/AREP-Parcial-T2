f0ce6e03dc6da09d53261ff450aa4acfe83bf668
OneCode182 <onecode0182@gmail.com>
2026-03-25 12:22:33 -0500

    feat: finish sample version of Proxy Server

--- ProxyServer/src/main/java/org/example/HttpConnectionExample.java
+++ ProxyServer/src/main/java/org/example/HttpConnectionExample.java
@@ -1,4 +1,9 @@
 package org.example;
+import jdk.jfr.Frequency;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -6,30 +11,109 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 
-
+@RestController
 public class HttpConnectionExample {
 
-    private static final String USER_AGENT = "Mozilla/5.0";
-    private static final String GET_URL = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=fb&apikey=Q1QZFVJQ21K7C6XM";
-
-    private static final String SERVICE_LINEAR_SEARCH = "http://ec2-100-28-225-246.compute-1.amazonaws.com:8080/linearsearch?list=10,70,80,2&value=80";
-
-    public static void main(String[] args) throws IOException {
+    private final String USER_AGENT = "Mozilla/5.0";
+    private final String SERVICE_EC2_N1 = "http://ec2-100-28-225-246.compute-1.amazonaws.com:8080";
+    private final String SERVICE_EC2_N2 = "";
 
 
+    private final String baseService1 = "/linearsearch";
+    private final String baseService2 = "/binarysearch";
+
+
+    @GetMapping(baseService1)
+    public String linearSearch(@RequestParam(value = "list") int[] items, @RequestParam(value = "value") int value) {
         try {
-            System.out.println("SERVER 1: LINEAR SEARCH");
-            StringBuffer respService1 = request(SERVICE_LINEAR_SEARCH);
-            System.out.println(respService1);
-
+            return requestLinearSearch(SERVICE_EC2_N1, items, value);
         } catch (Exception e) {
-            System.out.println("ERROR SERVER 1");
-        }
+            try {
+                return requestLinearSearch(SERVICE_EC2_N2, items, value);
+            } catch (Exception f) {
+                String msg = "ERROR SERVER 1 and 2 | NO SERVICE AVALAIBLE";
+                System.out.println(msg);
+                return msg;
+            }
 
+        }
     }
 
 
-    private static StringBuffer request(String serviceUrl) throws IOException {
+    @GetMapping(baseService2)
+    public String binarySearch(@RequestParam(value = "list") int[] items, @RequestParam(value = "value") int value) {
+        try {
+            return requestBinarySearch(SERVICE_EC2_N1, items, value);
+        } catch (Exception e) {
+            try {
+                return requestBinarySearch(SERVICE_EC2_N2, items, value);
+            } catch (Exception f) {
+                String msg = "ERROR SERVER 1 and 2 | NO SERVICE AVALAIBLE";
+                System.out.println(msg);
+                return msg;
+            }
+
+        }
+    }
+
+
+    private String requestLinearSearch(String machine, int[] items, int value)  throws IOException {
+        String url = "";
+        if (machine.equals(SERVICE_EC2_N1)) {
+            System.out.println("SERVER 1: LINEAR SEARCH");
+            url = buildUrl(SERVICE_EC2_N1, baseService1, items, value);
+        } else if (machine.equals(SERVICE_EC2_N2)) {
+            System.out.println("SERVER 2: LINEAR SEARCH");
+            url = buildUrl(SERVICE_EC2_N2, baseService1, items, value);
+        }
+
+        System.out.println("URL: " + url);
+
+        StringBuffer respService = request(url);
+        System.out.println(respService);
+        return respService.toString();
+    }
+
+
+    private String requestBinarySearch(String machine, int[] items, int value)  throws IOException {
+        String url = "";
+        if (machine.equals(SERVICE_EC2_N1)) {
+            System.out.println("SERVER 1: BINARY SEARCH");
+            url = buildUrl(SERVICE_EC2_N1, baseService2, items, value);
+        } else if (machine.equals(SERVICE_EC2_N2)) {
+            System.out.println("SERVER 2: BINARY SEARCH");
+            url = buildUrl(SERVICE_EC2_N2, baseService2, items, value);
+        }
+
+        StringBuffer respService = request(url);
+        System.out.println(respService);
+        return respService.toString();
+    }
+
+
+
+    private String intToStr(int[] items) {
+        String data = "";
+        for (int i = 0; i < items.length; i++) {
+            if (i < items.length - 1) {
+                data = data + items[i] + ",";
+            } else {
+                data = data + items[i];
+            }
+        }
+        return data;
+    }
+
+
+    private String buildUrl(String urlService, String baseService, int[] items, int value) {
+
+        return urlService + baseService + "?list=" + intToStr(items) + "&value=" + String.valueOf(value);
+    }
+
+
+
+
+    private StringBuffer request(String serviceUrl) throws IOException {
 
         URL obj = new URL(serviceUrl);
         HttpURLConnection con = (HttpURLConnection) obj.openConnection();
